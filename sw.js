792ff4cacb8014a5cf8b23e009b11e67252866a7
becausy <44943078+becausy@users.noreply.github.com>
2018-11-11 08:58:18 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,10 @@
 //asignar un nombre y versión al cache
 const CACHE_NAME = 'v1_cache_programador_fitness',
   urlsToCache = [
-    './',
     'https://fonts.googleapis.com/css?family=Raleway:400,700',
     'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
     'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
     'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
-    './style.css',
-    './script.js',
     './img/ProgramadorFitness.png',
     './img/favicon.png'
   ]
